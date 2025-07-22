6559710f14f257a4a7b826bf713a63d0e9194039
maicuti131 <ng.thanhthao0207e@gmail.com>
2025-07-22 07:27:42 +0700

    Update Hello.js

--- Discordbot/Commands/Hello.js
+++ Discordbot/Commands/Hello.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('Chào bot 👋'),
+  async execute(interaction) {
+    await interaction.reply('Chào bạn nha! ✨');
+  },
+};
