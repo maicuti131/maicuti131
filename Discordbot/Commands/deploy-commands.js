bba83da84641ec46321f34c36650e795496bd46e
maicuti131 <ng.thanhthao0207e@gmail.com>
2025-07-24 06:52:40 +0700

    Update deploy-commands.js

--- Discordbot/Commands/deploy-commands.js
+++ Discordbot/Commands/deploy-commands.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { REST, Routes } = require('discord.js');
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
